e936a2809f63a91eaa901ec789bb8bec37612cb7
Anton Kechashin <atk@sitecore.net>
2017-04-10 11:30:48 +0300

    Initial commit.

---
+++ src/Sitecore.Support.156773/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.156773")]
+[assembly: AssemblyProduct("Sitecore.Support.156773")]
+[assembly: ComVisible(false)]
